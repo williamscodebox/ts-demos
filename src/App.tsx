6c45a8ff960d1d1fb67810f685b698b2f84f4322
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-17 13:13:32 -0400

    added FetchTodo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import FetchTodo from "./components/FetchTodo";
 
 function App() {
   return (
     <div className="text-3xl font-bold underline">
       App with style from tailwinds css
+      <FetchTodo />
     </div>
   );
 }
---
+++ src/components/FetchTodo.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+type Todo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+function FetchTodo() {
+  const [data, setData] = useState<Todo>();
+
+  useEffect(() => {
+    const fetchDataAsync = async () => {
+      const data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+      if (!data.ok) {
+        throw new Error("Failed to fetch weather");
+      }
+      const data2 = await data.json();
+      setData(data2);
+    };
+    fetchDataAsync();
+  }, []);
+
+  return <div>{data ? data.title : "Loading..."}</div>;
+}
+
+export default FetchTodo;
