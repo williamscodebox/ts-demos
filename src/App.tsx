a0a937051019ae118506f833c1c92d007f7d93e4
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-17 20:38:38 -0400

    worked on styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import FetchTodo from "./components/FetchTodo";
+import Form from "./components/Form";
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
+    <div className="text-2xl">
       App with style from tailwinds css
       <FetchTodo />
+      <div>
+        <Form />
+      </div>
     </div>
   );
 }
