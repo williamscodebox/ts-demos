ee161eb7fc854589ed1ad3397703c85c80fdb9d9
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-19 14:48:34 -0400

    initial setup for eCommerce project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
+import Sidebar from "./eCommerceComponents/RealeCommerceComponents/Sidebar";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <div className="flex h-screen">
+        <Sidebar />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { FilterProvider } from "./providers/FilterContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <FilterProvider>
+      <App />
+    </FilterProvider>
   </StrictMode>
 );
