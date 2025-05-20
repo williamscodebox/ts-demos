3267f64c1154ffc9f043cf589896aded2be5eb14
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-19 20:27:16 -0400

    finished eCommerce project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Sidebar from "./eCommerceComponents/RealeCommerceComponents/Sidebar";
+import MainContent from "./eCommerceComponents/RealeCommerceComponents/MainContent";
+import ProductPage from "./eCommerceComponents/RealeCommerceComponents/ProductPage";
+import TopSellers from "./eCommerceComponents/RealeCommerceComponents/TopSellers";
+import PopularBlogs from "./eCommerceComponents/RealeCommerceComponents/PopularBlogs";
 
 function App() {
   return (
     <Router>
       <div className="flex h-screen">
-        <Sidebar />
+        <Sidebar />{" "}
+        <div className="rounded w-full flex justify-between flex-wrap">
+          <Routes>
+            <Route path="/" element={<MainContent />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+          </Routes>
+          <div>
+            <TopSellers />
+            <PopularBlogs />
+          </div>
+        </div>
       </div>
     </Router>
   );
