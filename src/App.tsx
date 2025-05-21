483996c4ee37a1e0d1ddaf81cb12decba311a71f
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-21 00:33:31 -0400

    updated App

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,23 @@ function App() {
           animate={{ x: 800 }}
           transition={{ delay: 0.25, ease: "easeOut", duration: 0.25 }}
         ></motion.div>
+        <motion.div
+          className="boxTwo"
+          animate={{
+            scale: [1, 1.05, 1],
+            // rotate: [0, 0, 270, 270, 0],
+            borderRadius: ["8%", "10%", "8%"],
+            zIndex: 1,
+            opacity: [0, 1, 0],
+          }}
+          transition={{
+            // delay: 0.25,
+            ease: "linear",
+            duration: 1.2,
+            repeat: Infinity,
+          }}
+        ></motion.div>
+        <div className="btn-text">Click Me</div>
       </div>
     </div>
   );
