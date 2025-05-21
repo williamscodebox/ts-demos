8420901369a3b7772d14190a79ac66b5c564630f
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-20 22:18:47 -0400

    added new setup for framerMotion project

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,7 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
-import Sidebar from "./eCommerceComponents/RealeCommerceComponents/Sidebar";
-import MainContent from "./eCommerceComponents/RealeCommerceComponents/MainContent";
-import ProductPage from "./eCommerceComponents/RealeCommerceComponents/ProductPage";
-import TopSellers from "./eCommerceComponents/RealeCommerceComponents/TopSellers";
-import PopularBlogs from "./eCommerceComponents/RealeCommerceComponents/PopularBlogs";
-import useMediaQuery from "@mui/material/useMediaQuery";
 
 function App() {
-  const requiredWidth = useMediaQuery("(min-width:1024px)");
-  return requiredWidth ? (
-    <Router>
-      <div className="flex h-screen">
-        <Sidebar />
-        <div className="rounded w-fit flex justify-between ">
-          <Routes>
-            <Route path="/" element={<MainContent />} />
-            <Route path="/product/:id" element={<ProductPage />} />
-          </Routes>
-          <div>
-            <TopSellers />
-            <PopularBlogs />
-          </div>
-        </div>
-      </div>
-    </Router>
-  ) : (
-    <p>We do not fully support your device </p>
-  );
+  return <div>App</div>;
 }
 
 export default App;
