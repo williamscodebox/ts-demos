a2d823dda7b8a3d8d577d7400ddd60ba16cbf3c9
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-20 22:45:03 -0400

    updated App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import { motion } from "motion/react";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <div className="box-start">
+        App
+        <motion.div
+          className="box"
+          initial={{ x: 0 }}
+          animate={{ x: 800 }}
+          transition={{ delay: 0.25, ease: "easeOut", duration: 0.25 }}
+        ></motion.div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
